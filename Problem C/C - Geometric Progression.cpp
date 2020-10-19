13ea33a9d4ea1d48311c8780fe6f93004ddbbfe0
Rajan-226 <56467449+Rajan-226@users.noreply.github.com>
2020-10-19 15:31:10 +0530

    Create C - Geometric Progression.cpp

---
+++ Problem C/C - Geometric Progression.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+#define mxsize (int)1e6+5
+ll fastexp(ll a,ll n,ll nod=LLONG_MAX){
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+void fun(){
+    int n,k;
+    cin>>n>>k;
+    vector<ll> a(n,0),left(n,0);
+    fi(0,n) cin>>a[i];
+    unordered_map<ll,int> mp;
+    fi(0,n){
+        if(a[i]%k==0)   left[i]=mp[a[i]/k];
+        mp[a[i]]++;
+    }
+    mp.clear();
+    ll ans=0;
+    for(int i=n-1;i>=0;i--){
+        ans+=mp[a[i]*k]*left[i];
+        mp[a[i]]++;
+    }
+    cout<<ans<<"\n";
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout.precision(20);
+    cout<<fixed;
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    while(t--){
+        fun();
+    }
+}
