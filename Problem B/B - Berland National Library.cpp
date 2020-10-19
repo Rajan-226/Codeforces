437e78cd7e6056e4d83b33c6b958e80f43c98915
Rajan-226 <56467449+Rajan-226@users.noreply.github.com>
2020-10-19 15:30:31 +0530

    Create B - Berland National Library.cpp

---
+++ Problem B/B - Berland National Library.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+#define mxsize (int)1e6+5
+ll fastexp(ll a,ll n,ll nod=LLONG_MAX){
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+void fun(){
+    int n;
+    cin>>n;
+    set<int> s;
+    int ans=0,now=0;
+    fi(0,n){
+        char c;
+        int d;
+        cin>>c>>d;
+        auto it=s.find(d);
+        if(c=='+'){
+            s.insert(d);
+            now+=1;
+            ans=max(ans,now);
+        }
+        else{
+            if(it!=s.end()){
+                now--;
+            }
+            else{
+                s.insert(d);
+                ans++;
+            }
+        }
+    }
+    cout<<ans<<"\n";
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout.precision(20);
+    cout<<fixed;
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    while(t--){
+        fun();
+    }
+}
